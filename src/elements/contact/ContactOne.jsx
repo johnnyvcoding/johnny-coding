245a5468398485a23d0fc75561f9ba5f1a81c3c1
John Vazquez <johnnyvcoding@gmail.com>
2021-04-02 20:06:13 -0500

    netlify-t

--- src/elements/contact/ContactOne.jsx
+++ src/elements/contact/ContactOne.jsx
@@ -1,85 +1,121 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class ContactOne extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            rnName: '',
-            rnEmail: '',
-            rnSubject: '',
-            rnMessage: '',
-        };
-    }
-    render(){
-        return(
-            <div className="contact-form--1">
-                <div className="container">
-                    <div className="row row--35 align-items-start">
-                        <div className="col-lg-6 order-2 order-lg-1">
-                            <div className="section-title text-left mb--50">
-                                <h2 className="title">Contact Me.</h2>
-                                <p className="description">I am available for  work or for questions! Connect with me via phone: <a href="tel:+12246278147">224-627-8147</a> or email:
-                                    <a href="mailto:me@johnny.com"> me@johnnycoding.com</a> </p>
-                            </div>
-                            <div className="form-wrapper">
+class ContactOne extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			rnName: '',
+			rnEmail: '',
+			rnSubject: '',
+			rnMessage: '',
+		};
 
-                                <form name="contact" data-netlify="true" method="POST">
-                                    <input type="hidden"  value="contact"  name="form-name" />
-                                    <label htmlFor="item01">
-                                        <input
-                                            type="text"
-                                            name="name"
-                                            id="item01"
-                                            value={this.state.rnName}
-                                            onChange={(e)=>{this.setState({rnName: e.target.value});}}
-                                            placeholder="Your Name *"
-                                        />
-                                    </label>
+		// this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-                                    <label htmlFor="item02">
-                                        <input
-                                            type="text"
-                                            name="email"
-                                            id="item02"
-                                            value={this.state.rnEmail}
-                                            onChange={(e)=>{this.setState({rnEmail: e.target.value});}}
-                                            placeholder="Your email *"
-                                        />
-                                    </label>
+	// handleSubmit(e) {
+	// 	e.preventDefault();
 
-                                    <label htmlFor="item03">
-                                        <input
-                                            type="text"
-                                            name="subject"
-                                            id="item03"
-                                            value={this.state.rnSubject}
-                                            onChange={(e)=>{this.setState({rnSubject: e.target.value});}}
-                                            placeholder="Write a Subject"
-                                        />
-                                    </label>
-                                    <label htmlFor="item04">
-                                        <textarea
-                                            type="text"
-                                            id="item04"
-                                            name="message"
-                                            value={this.state.rnMessage}
-                                            onChange={(e)=>{this.setState({rnMessage: e.target.value});}}
-                                            placeholder="Your Message"
-                                        />
-                                    </label>
-                                    <button className="rn-button-style--2 btn-solid" type="submit" value="submit" name="submit" id="mc-embedded-subscribe">Submit</button>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 order-1 order-lg-2">
-                            <div className="thumbnail mb_md--30 mb_sm--30">
-                                <img src="/assets/images/about/maingdl.JPG" alt="Me in Guadalajara, Jalisco, Mexico"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	// 	fetch('/', {
+	// 		method: 'POST',
+	// 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+	// 	})
+	// 		.then(() => console.log('Form successfully submitted'))
+	// 		.catch((error) => alert(error));
+	// }
+
+	render() {
+		return (
+			<div className='contact-form--1'>
+				<div className='container'>
+					<div className='row row--35 align-items-start'>
+						<div className='col-lg-6 order-2 order-lg-1'>
+							<div className='section-title text-left mb--50'>
+								<h2 className='title'>Contact Me.</h2>
+								<p className='description'>
+									I am available for work or for questions! Connect with me via
+									phone: <a href='tel:+12246278147'>224-627-8147</a> or email:
+									<a href='mailto:me@johnny.com'> me@johnnycoding.com</a>{' '}
+								</p>
+							</div>
+							<div className='form-wrapper'>
+								<form name='contact' data-netlify='true' method='POST'>
+									<input type='hidden' value='contact' name='form-name' />
+									<label htmlFor='item01'>
+										<input
+											type='text'
+											name='name'
+											id='item01'
+											value={this.state.rnName}
+											onChange={(e) => {
+												this.setState({ rnName: e.target.value });
+											}}
+											placeholder='Your Name *'
+										/>
+									</label>
+
+									<label htmlFor='item02'>
+										<input
+											type='text'
+											name='email'
+											id='item02'
+											value={this.state.rnEmail}
+											onChange={(e) => {
+												this.setState({ rnEmail: e.target.value });
+											}}
+											placeholder='Your email *'
+										/>
+									</label>
+
+									<label htmlFor='item03'>
+										<input
+											type='text'
+											name='subject'
+											id='item03'
+											value={this.state.rnSubject}
+											onChange={(e) => {
+												this.setState({ rnSubject: e.target.value });
+											}}
+											placeholder='Write a Subject'
+										/>
+									</label>
+									<label htmlFor='item04'>
+										<textarea
+											type='text'
+											id='item04'
+											name='message'
+											value={this.state.rnMessage}
+											onChange={(e) => {
+												this.setState({ rnMessage: e.target.value });
+											}}
+											placeholder='Your Message'
+										/>
+									</label>
+									<button
+										className='rn-button-style--2 btn-solid'
+										type='submit'
+										value='submit'
+										name='submit'
+										id='mc-embedded-subscribe'
+                                        onClick={(e) => e.preventDefault()}
+									>
+										Submit
+									</button>
+								</form>
+							</div>
+						</div>
+						<div className='col-lg-6 order-1 order-lg-2'>
+							<div className='thumbnail mb_md--30 mb_sm--30'>
+								<img
+									src='/assets/images/about/maingdl.JPG'
+									alt='Me in Guadalajara, Jalisco, Mexico'
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 export default ContactOne;
